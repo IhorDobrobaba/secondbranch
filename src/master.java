3bca4939c1d2838a34c7b28e7cfacc90c3abb796
IhorDobrobaba <dobrobov@gmail.com>
2018-11-11 12:13:20 +0200

    commit1

---
+++ src/master.java
@@ -0,0 +1,7 @@
+public class master {
+    public static void main(String[] args){
+        System.out.println("sssdada");
+        System.out.println("sssdada");
+        System.out.println("sssdada");
+    }
+}
